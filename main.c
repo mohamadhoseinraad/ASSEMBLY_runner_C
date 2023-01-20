26afd10b52a7c55dc64b32d39048f51b3f3a85ae
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2023-01-20 20:25:21 +0330

    complate version 1

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 
 void readF(FILE *stream, char buf[][100]);
 
+
 int count1s(int N);
 int ADD(int* S,int indexF, int index1, int index2);
 int SUB(int* S,int indexF, int index1, int index2);
@@ -22,8 +23,8 @@ int XORI(int* S,int indexF, int index1, int number);
 int ORI(int* S,int indexF, int index1, int number);
 
 void SWP(int* S,int index1, int index2);
-void DUMP_REGS(int* S);
-void DUMP_REGS_F(int* S);
+void DUMP_REGS(int* S, int* SR);
+void DUMP_REGS_F(int* S, int* SR);
 void INPUT(int*S);
 void OUTPUT(int*S);
 
@@ -31,6 +32,8 @@ void update_reg(int*SR , int var);
 
 int main()
 {
+    int S[32];
+    int SR[8];
     char buf[100][100];
     FILE *stream = fopen("in.txt", "r");
     readF(stream, buf);
@@ -38,13 +41,14 @@ int main()
 
     for (int i = 0; buf[i][0] != '\0'; i++)
     {
+        int a,b,d;
         char cmd[100];
         sscanf(buf[i], "%s", cmd);
 
         
         if (strcmp(cmd, "ADD") == 0 || strcmp(cmd, "SUB") == 0 || strcmp(cmd, "AND") == 0 || strcmp(cmd, "XOR") == 0 || strcmp(cmd, "OR") == 0)
         {
-            int a,b,d;
+            
             int check = sscanf(buf[i],"%s S%d, S%d, S%d",cmd,&a,&b,&d);
             if (check != 4)
             {
@@ -52,12 +56,44 @@ int main()
             }
             else
             {
-                printf("%s %d %d %d\n",cmd,a,b,d);
+                if (strcmp(cmd, "AND") == 0)
+                {
+                    AND(S,a,b,d);
+                    update_reg(SR,S[a]);
+                }
+                if (strcmp(cmd, "SUB") == 0)
+                {
+                    int overf = SUB(S,a,b,d);
+                    update_reg(SR,S[a]);
+                    if (overf == 1)
+                        SR[5] = 1;
+                    else
+                        SR[5] = 0;
+                }
+                if (strcmp(cmd, "ADD") == 0)
+                {
+                    int overf = ADD(S,a,b,d);
+                    update_reg(SR,S[a]);
+                    if (overf == 1)
+                        SR[5] = 1;
+                    else
+                        SR[5] = 0;
+                }
+                if (strcmp(cmd, "XOR") == 0)
+                {
+                    XOR(S,a,b,d);
+                    update_reg(SR,S[a]);
+                }
+                if (strcmp(cmd, "OR") == 0)
+                {
+                    OR(S,a,b,d);
+                    update_reg(SR,S[a]);
+                }
             }
         }
         else if (strcmp(cmd, "ADDI") == 0 || strcmp(cmd, "SUBI") == 0 || strcmp(cmd, "ANDI") == 0 || strcmp(cmd, "XORI") == 0 || strcmp(cmd, "ORI") == 0)
         {
-            int a,b,d;
+            
             int check = sscanf(buf[i],"%s S%d, S%d, %d",cmd,&a,&b,&d);
             if (check != 4)
             {
@@ -65,12 +101,44 @@ int main()
             }
             else
             {
-                printf("%s %d %d %d\n",cmd,a,b,d);
+                if (strcmp(cmd, "ANDI") == 0)
+                {
+                    ANDI(S,a,b,d);
+                    update_reg(SR,S[a]);
+                }
+                if (strcmp(cmd, "SUBI") == 0)
+                {
+                    int overf = SUBI(S,a,b,d);
+                    update_reg(SR,S[a]);
+                    if (overf == 1)
+                        SR[5] = 1;
+                    else
+                        SR[5] = 0;
+                }
+                if (strcmp(cmd, "ADDI") == 0)
+                {
+                    int overf = ADDI(S,a,b,d);
+                    update_reg(SR,S[a]);
+                    if (overf == 1)
+                        SR[5] = 1;
+                    else
+                        SR[5] = 0;
+                }
+                if (strcmp(cmd, "XORI") == 0)
+                {
+                    XORI(S,a,b,d);
+                    update_reg(SR,S[a]);
+                }
+                if (strcmp(cmd, "ORI") == 0)
+                {
+                    ORI(S,a,b,d);
+                    update_reg(SR,S[a]);
+                }
             }
         }
         else if (strcmp(cmd, "SWP") == 0 )
         {
-            int a,b;
+            
             int check = sscanf(buf[i],"%s S%d, S%d",cmd,&a,&b);
             if (check != 3)
             {
@@ -78,12 +146,12 @@ int main()
             }
             else
             {
-                printf("%s %d %d\n",cmd,a,b);
+                SWP(S,a,b);
             }
         }
         else if (strcmp(cmd, "MOV") == 0 )
         {
-            int a,b;
+            
             int check = sscanf(buf[i],"%s S%d, S%d",cmd,&a,&b);
             int check2 = sscanf(buf[i],"%s S%d, %d",cmd,&a,&b);
             if (check != 3 && check2 !=3)
@@ -93,19 +161,52 @@ int main()
             }
             else if(check2 == 3)
             {
-                printf("%s %d %d\n",cmd,a,b);
+                S[a] = b;
             }
             else
             {
-                printf("%s %d S%d\n",cmd,a,b);
+                S[a] = S[b];
             }
             
         }
-        else if (strcmp(cmd, "JMP") == 0 || strcmp(cmd, "OUTPUT") == 0 || strcmp(cmd, "INPUT") == 0 || strcmp(cmd, "DUMP_REGS") == 0 || strcmp(cmd, "DUMP_REGS_F") == 0)
+        else if (strcmp(cmd, "JMP") == 0 )
+        {
+            int check = sscanf(buf[i],"%s %d",cmd,&a);
+            if (check != 2 )
+            {
+                
+                printf("Erro use this cmd like this: %s 2\n",cmd);
+            }
+            else
+            {
+                i = a - 1;
+            }
+            
+        }
+        else if (strcmp(cmd, "OUTPUT") == 0 || strcmp(cmd, "INPUT") == 0 || strcmp(cmd, "DUMP_REGS") == 0 || strcmp(cmd, "DUMP_REGS_F") == 0)
         {
             sscanf(buf[i],"%s",cmd);
-            printf("%s \n",cmd);
-            
+            if (strcmp(cmd, "OUTPUT") == 0)
+            {
+                OUTPUT(S);
+            }
+            else if (strcmp(cmd, "INPUT") == 0)
+            {
+                INPUT(S);
+            }
+            else if (strcmp(cmd, "DUMP_REGS") == 0)
+            {
+                DUMP_REGS(S,SR);
+            }
+            else if (strcmp(cmd, "DUMP_REGS_F") == 0)
+            {
+                DUMP_REGS_F(S,SR);
+            }
+                        
+        }
+        else if (strcmp(cmd, "EXIT") == 0 )
+        {
+            return 0;
         }
         else
         {
@@ -229,16 +330,21 @@ void SWP(int* S,int index1, int index2)
     *(S+index1) = *(S+index2);
     *(S+index2) = temp;
 }
-void DUMP_REGS(int* S)
+void DUMP_REGS(int* S, int* SR)
 {
     printf("Register : \n ");
     for (int i = 0; i < 32; i++)
     {
         printf(" | %d : %d | ",i, *(S+i));
     }
+    printf("\nRegister  Status: \n ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf(" | %d : %d | ",i, *(SR+i));
+    }
     
 }
-void DUMP_REGS_F(int* S)
+void DUMP_REGS_F(int* S, int* SR)
 {
     FILE *output;
     output = fopen("regs.txt","w");
@@ -246,6 +352,11 @@ void DUMP_REGS_F(int* S)
     for (int i = 0; i < 32; i++)
     {
         fprintf(output, " | %d : %d | ",i, *(S+i));
+    }
+    fprintf(output, "\nRegister Status : \n ");
+    for (int i = 0; i < 32; i++)
+    {
+        fprintf(output, " | %d : %d | ",i, *(SR+i));
     }
     fclose(output);
 }
@@ -320,3 +431,4 @@ void readF(FILE *stream, char buf[][100])
         }
     }
 }
+
