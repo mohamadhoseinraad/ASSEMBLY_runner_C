a9368cc130514db32ba800cc8bffb1819236f10f
Mohammad hosein Shabanirad <mh.shabanirad@icloud.com>
2023-01-20 18:15:41 +0330

    add read from array and check simple errors

--- Getf.c
+++ Getf.c
@@ -11,10 +11,7 @@ int main()
         {
             buf[l][k] = '\0';
         }
-    }
-    
-    
-    int a,b;
+    }  
     char c;
     FILE *stream = fopen("in.txt", "r");
     int i =0,j=0;
@@ -47,9 +44,22 @@ int main()
         }
         else
         {
-            buf[i][j] = c;
+            buf[i][j] = toupper(c);
             j++;
         }
+    }
+    char cmd[100];
+    sscanf(buf[0], "%s", cmd);
+    printf("%s\n",cmd);
+    int a,b,d;
+    int check = sscanf(buf[0],"%s S%d, S%d, S%d",cmd,&a,&b,&d);
+    if (check != 4)
+    {
+        printf("Erro\n");
+    }
+    else
+    {
+        printf("%s %d %d %d\n",cmd,a,b,d);
     }
     
     
